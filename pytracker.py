e086814e796c6ac1b416ad7bce519841cb9e4e59
dalibor91 <dalibor_menkovic@hotmail.com>
2017-06-21 10:14:43 +0200

    Add ignore
    Adding ignore and unignore options

--- pytracker.py
+++ pytracker.py
@@ -10,13 +10,16 @@ program = os.path.basename(sys.argv[0])
 if len(sys.argv) < 3:
         print """
 Usage
-        %s [directory] [extension] [-d]|[-history]|[-history <runid>]
+        %s [directory] [extension] <options>
 Example
-        %s '/home/' '.php'                             - to only check for new files and
-        %s '/home/' '.php' -d                          - to track if file is deleted
-        %s '/home/' '.php' -history                    - to list history dates with uuid
-        %s '/home/' '.php' -history '12345-12345-1234' - to see history for that specific uuid
-        """ % (program, program, program, program, program)
+        %s '/home/' '.php'                                - to only check for new files and
+        %s '/home/' '.php' -d                             - to track if file is deleted
+        %s '/home/' '.php' -history                       - to list history dates with uuid
+        %s '/home/' '.php' -history '12345-12345-1234'    - to see history for that specific uuid
+	%s '/home/' '.php' -ignore '/home/path_to_file'   - to ignore file
+	%s '/home/' '.php' -unignore '/home/path_to_file' - to remote file from ignore list
+	%s '/home/' '.php' -ignore-list                   - to list all ignored files
+        """ % (program, program, program, program, program, program, program, program)
 
         quit();
 
@@ -52,6 +55,7 @@ if not os.path.isfile(targetDB):
                 on_change TEXT NULL,
                 on_delete TEXT NULL,
                 deleted INTEGER DEFAULT 0,
+		ignore INTEGER DEFAULT 0,
                 created DATETIME DEFAULT CURRENT_TIMESTAMP
         );
         """)
@@ -83,15 +87,45 @@ if otherArguments == 'history':
         if historyAvailible is False:
                 print "History is not availible for given arguments"
                 quit();
-                
+
         if len(sys.argv) == 4:
                 print "%s | %s | %s" % ( "Files No.".ljust(10), "History ID".ljust(40), "Date".ljust(16) )
-                for i in curr.execute(" SELECT COUNT(*), h.runid, h.created  FROM history h INNER JOIN files f ON f.id = h.file_id GROUP BY h.runid"):
+                for i in curr.execute(" SELECT COUNT(*), h.runid, h.created  FROM history h INNER JOIN files f ON f.id = h.file_id GROUP BY h.runid ORDER BY h.created ASC"):
                         print "%s | %s | %s" % ( str(i[0]).ljust(10), i[1].ljust(40), i[2].ljust(16) )
         elif len(sys.argv) == 5:
                 for i in curr.execute(" SELECT f.path, h.fnew, h.fchd, h.fdel, h.created FROM history h INNER JOIN files f ON f.id = h.file_id WHERE h.runid = ? ", (sys.argv[4], )):
                         print "%s %s" % (( 'Modified' if i[2] == 1 else ('New' if i[1] == 1 else 'Deleted' )), i[0])
         quit();
+
+
+if otherArguments == 'ignore-list':
+	print "Files ignored:"
+	for i in curr.execute(" SELECT path FROM files WHERE ignore = 1"):
+		print "%s" % i[0]
+	quit()
+
+if otherArguments == 'ignore':
+	if (len(sys.argv) != 5):
+		print "Need file path to ignore"
+	else:
+		print "Ignoring file %s " % sys.argv[4]
+		curr.execute("UPDATE files SET ignore = 1 WHERE path = ? ", (sys.argv[4], ))
+
+	conn.commit();
+	conn.close();
+	quit();
+
+if otherArguments == 'unignore':
+        if (len(sys.argv) != 5):
+                print "Need file path to unignore"
+        else:
+		print "Unignoring file %s " % sys.argv[4]
+                curr.execute("UPDATE files SET ignore = 0 WHERE path = ? ", (sys.argv[4], ))
+        conn.commit();
+        conn.close();
+        quit();
+
+
 
 for root, subdirs, files in os.walk(targetDir):
         for f in files:
@@ -109,7 +143,8 @@ for root, subdirs, files in os.walk(targetDir):
                                 f.id,
                                 f.path,
                                 h.hash,
-                                f.on_change
+                                f.on_change,
+				f.ignore
                         FROM
                                 files AS f
                         INNER JOIN
@@ -131,7 +166,7 @@ for root, subdirs, files in os.walk(targetDir):
                                 file_id = curr.lastrowid
                                 curr.execute("INSERT INTO hashes (file_id, hash) VALUES (? , ?)", (file_id, hashed, ))
                                 curr.execute("INSERT INTO history (runid, file_id, fnew) VALUES ( ?, ?, 1)", (runuuid, file_id,))
-                        elif result[2] != hashed:
+                        elif (result[2] != hashed) and (result[4] == 0):
                                 #hash is not same so its different
                                 #insert only in hashed
                                 print "Modified %s " % absFile
